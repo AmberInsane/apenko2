a02199b7c8329e4cd4f6c1d15ed3830aefc5c740
Mary Stankevich <marrycitty@mail.ru>
2022-06-08 20:26:20 +0200

    Update HelloMikhail.java

--- src/Homework_1/Task_1/HelloMikhail.java
+++ src/Homework_1/Task_1/HelloMikhail.java
@@ -1,3 +1,5 @@
+//+, но не хватает строки c package. этот код работает?
+//имя пакета должно быть маленькими буквами
 public class HelloMikhail {
     public static void main(String[] args)  {
         System.out.println("Hello Mikhail");
